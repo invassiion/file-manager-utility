cdd4223aad71bb660772a4860950513b6cb377db
invassiion <invassiion@gmail.com>
2024-07-24 19:23:18 +0700

    Логи пишет только jar из директории out, в директории target работает, но без логов.

---
+++ src/test/java/FileFilteringTest.java
@@ -0,0 +1,12 @@
+import com.example.filemanager.FileFiltering;
+import org.junit.jupiter.api.BeforeEach;
+
+public class FileFilteringTest {
+
+    private FileFiltering fileFiltering;
+
+    @BeforeEach
+    void setUp() {
+        fileFiltering = new FileFiltering();
+    }
+}
